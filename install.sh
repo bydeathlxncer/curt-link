de924911fdf2ad22ab8ed6bd8ec3731dc4828493
bydeathlxncer <83184525+bydeathlxncer@users.noreply.github.com>
2021-08-29 16:00:03 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+pkg install python -y 
+pip install pyshorteners
+pip install colorama
+python url.py
---
+++ url.py
@@ -0,0 +1,58 @@
+#albania passo a picakkkkkkkkk
+#se vc e da pcw quero q vc se foda
+#fuckingwordplugg19shit!!
+#lursy boiola
+#@bydeathlxncer
+'''
+coded by:                            
+_         _         _   _   _
+| |_ _ _ _| |___ ___| |_| |_| |_ _ ___ ___ ___ ___
+| . | | | . | -_| .'|  _|   | |_'_|   |  _| -_|  _|
+|___|_  |___|___|__,|_| |_|_|_|_,_|_|_|___|___|_|
+    |___|
+albania fdsjkkkkk
+'''
+
+import time
+import pyshorteners
+import os
+from colorama import Fore,Back,Style
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  
+        command = 'cls'
+    os.system(command)
+clearConsole()
+
+print(Fore.LIGHTYELLOW_EX+'''
+
+  █████▒█    ██  ▄████▄   ██ ▄█▀    █    ██  ██▀███   ██▓    
+▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒     ██  ▓██▒▓██ ▒ ██▒▓██▒    
+▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░    ▓██  ▒██░▓██ ░▄█ ▒▒██░    
+░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄    ▓▓█  ░██░▒██▀▀█▄  ▒██░    
+░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄   ▒▒█████▓ ░██▓ ▒██▒░██████▒
+ ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒   ░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░░ ▒░▓  ░
+ ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░   ░░▒░ ░ ░   ░▒ ░ ▒░░ ░ ▒  ░
+ ░ ░    ░░░ ░ ░ ░        ░ ░░ ░     ░░░ ░ ░   ░░   ░   ░ ░   
+          ░     ░ ░      ░  ░         ░        ░         ░  ░
+                ░                                            
+''')
+url = input(Fore.LIGHTGREEN_EX+"[@] insira o link >>> "+Fore.LIGHTRED_EX)
+
+shrt = pyshorteners.Shortener()
+
+nurl = shrt.tinyurl.short(url) 
+
+print(Fore.LIGHTGREEN_EX+"[@] novo link >>> " +Fore.LIGHTBLUE_EX+nurl)
+
+time.sleep(4)
+print(Fore.LIGHTMAGENTA_EX+'''
+coded by:                            
+_         _         _   _   _
+| |_ _ _ _| |___ ___| |_| |_| |_ _ ___ ___ ___ ___
+| . | | | . | -_| .'|  _|   | |_'_|   |  _| -_|  _|
+|___|_  |___|___|__,|_| |_|_|_|_,_|_|_|___|___|_|
+    |___|
+
+ ''' )
+
